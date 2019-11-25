b0318758ea16ee6e2de0ee24d249187592057c2c
Goldpm <lowedavidadam@gmail.com>
2019-11-25 03:43:56 -0500

    added inquirer prompt

--- tracker.js
+++ tracker.js
@@ -3,35 +3,38 @@ const inquirer = require("inquirer");
 const cTable = require("console.table")
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "rootroot",
-  database: "employees_db"
+    // Your password
+    password: "rootroot",
+    database: "employees_db"
 });
 
+
 console.log(
-" ███████╗███╗   ███╗██████╗ ██╗      ██████╗ ██╗   ██╗███████╗███████╗       \n",
+    " ███████╗███╗   ███╗██████╗ ██╗      ██████╗ ██╗   ██╗███████╗███████╗       \n",
     "██╔════╝████╗ ████║██╔══██╗██║     ██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝        \n",
     "█████╗  ██╔████╔██║██████╔╝██║     ██║   ██║ ╚████╔╝ █████╗  █████╗          \n",
     "██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██╔══╝  ██╔══╝          \n",
     "███████╗██║ ╚═╝ ██║██║     ███████╗╚██████╔╝   ██║   ███████╗███████╗        \n",
     "╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝        \n",
-                                                                                 "\n",
-     "███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗██████╗        \n",
+    "\n",
+    "███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗██████╗        \n",
     "████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔════╝██╔══██╗       \n",
     "██╔████╔██║███████║██╔██╗ ██║███████║██║  ███╗█████╗  ██████╔╝       \n",
     "██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗       \n",
     "██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║       \n",
-     "╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝       \n");
+    "╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝       \n");
 
 inquirerInit();
+
+
 
 
 function inquirerInit() {
@@ -44,63 +47,71 @@ function inquirerInit() {
                 "View all employees",
                 "View all departments",
                 "View all roles",
-                "Add Employee",
-                "Remove Employee",
-                "Update Employee Role",
-                "Update Employee Manager"
+                "Alter Employee List",
+                "Update Employee List"
             ]
         }
-    ]).then(function (response) {
-        const action = response.action;
-        if (action === "Add Employee" || "Remove Employee") {inquirerEmploy();}
-        switch (action) {
-            case "View all employees":
-                viewAll('employ');
-                break;
-            case "View all departments":
-                viewAll('depart');
-                break;
-            case "View all roles":
-                viewAll('roles');
-                break;
-            case "Add Employee":
-                add();
-                break;
-            case "Remove Employee":
-                remove();
-                break;
-            case "Update Employee Role":
-                update('role');
-                break;
-            case "Update Employee Manager":
-                update('manage');
-                break;
-            default:
-                console.log("Error: Can't Determine Choice");
-                break;
+    ]).then(function (res) {
+        const action = res.action;
+        if (action === "Alter Employee List") {
+            let employList = mysql('SELECT first_name,last_name FROM employee');
+
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "Would you like to Add or Remove an Employee?",
+                    name: "addOrRemove",
+                    choices: [
+                        "Add Employee",
+                        "Remove Employee"
+                    ]
+                },
+                {
+                    type: "list",
+                    message: "Would you like to Add or Remove an Employee?",
+                    name: "action",
+                    choices: employList
+                },
+            ]).then(function (res) {
+                const action = response.action;
+                if (action === "Add Employee" || "Remove Employee") {inquirerEmploy();}
+                switch (action) {
+                    case "View all employees":
+                        viewAll('employ');
+                        break;
+                    case "View all departments":
+                        viewAll('depart');
+                        break;
+                    case "View all roles":
+                        viewAll('roles');
+                        break;
+                    case "Add Employee":
+                        add();
+                        break;
+                    case "Remove Employee":
+                        remove();
+                        break;
+                    case "Update Employee Role":
+                        update('role');
+                        break;
+                    case "Update Employee Manager":
+                        update('manage');
+                        break;
+                    default:
+                        console.log("Error: Can't Determine Choice");
+                        break;
+                }
+                });
+
+            });
         }
+
     });
 }
-inquirer.prompt([
-    {
-        type: "list",
-        message: "What would you like to do?",
-        name: "action",
-        choices: [
-            "View all employees",
-            "View all departments",
-            "View all roles",
-            "Add Employee",
-            "Remove Employee",
-            "Update Employee Role",
-            "Update Employee Manager"
-        ]
-    }
-]);
 
 function viewAll(type) {
     switch (type) {
-        
+
         case 'employ':
             sqlQuery("SELECT employee.id,first_name,last_name,title,department,manager FROM employee INNER JOIN role ON employee.role = role.title;");
             break;
@@ -112,23 +123,19 @@ function viewAll(type) {
         case 'roles':
             sqlQuery("SELECT * FROM role");
             break;
-    
+
         default:
             break;
     }
 }
 
 function add() {
-    
-}
 
-function remove() {
-    
 }
 
 function update(type) {
     switch (type) {
-        
+
         case 'role':
             sqlQuery("-->SELECT employee.id,first_name,last_name,title,department,manager FROM employee INNER JOIN role ON employee.role = role.title;");
             break;
@@ -143,12 +150,12 @@ function update(type) {
 }
 
 function sqlQuery(request) {
-    connection.query(request, function(err, res) {
+    connection.query(request, function (err, res) {
         if (err) throw err;
         console.log('\n');
         console.table(res);
         inquirerInit();
-      });
+    });
 }
 // inquirer.prompt([
 //     {
