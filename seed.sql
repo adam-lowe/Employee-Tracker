a8085927b34da0814914e7d1c0e86c723cf9e059
Goldpm <lowedavidadam@gmail.com>
2019-11-24 23:09:14 -0500

    added more inserts to tables

--- seed.sql
+++ seed.sql
@@ -16,33 +16,62 @@ CREATE TABLE employee (
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30),
-  salary DECIMAL,
   department VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
   id INT AUTO_INCREMENT,
-  name VARCHAR(30)
+  name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
-INSERT INTO music (first_name, last_name, title, department, salary, manager)
+INSERT INTO department (name)
+VALUES ("Sales");
+
+INSERT INTO department (name)
+VALUES ("Engineering");
+
+INSERT INTO department (name)
+VALUES ("Finance");
+
+INSERT INTO department (name)
+VALUES ("Sales Lead");
+
+INSERT INTO role (title, department)
+VALUES ("Sales Lead", "Sales");
+
+INSERT INTO role (title, department)
+VALUES ("Salesperson", "Sales");
+
+INSERT INTO role (title, department)
+VALUES ("Lead Engineer", "Engineering");
+
+INSERT INTO role (title, department)
+VALUES ("Software Engineer", "Engineering");
+
+INSERT INTO role (title, department)
+VALUES ("Accountant", "Finance");
+
+INSERT INTO role (title, department)
+VALUES ("Legal Team Lead", "Legal");
+
+INSERT INTO employee (first_name, last_name, title, department, salary, manager)
 VALUES ("John", "Doe", "Sales Lead", "Sales", 100000, "Ashley Rodriguez");
 
-INSERT INTO music (first_name, last_name, title, department, salary, manager)
+INSERT INTO employee (first_name, last_name, title, department, salary, manager)
 VALUES ("Mike", "Chan", "Salesperson", "Sales", 80000, "Ashley Rodriguez");
 
-INSERT INTO music (first_name, last_name, title, department, salary, manager)
+INSERT INTO employee (first_name, last_name, title, department, salary, manager)
 VALUES ("Ashley", "Rodriguez", "Lead Engineer", "Engineering", 150000, "Ashley Rodriguez");
 
-INSERT INTO music (first_name, last_name, title, department, salary, manager)
+INSERT INTO employee (first_name, last_name, title, department, salary, manager)
 VALUES ("Kevin", "Tupik", "Software Engineer", "Engineering", 120000, "Ashley Rodriguez");
 
-INSERT INTO music (first_name, last_name, title, department, salary, manager)
+INSERT INTO employee (first_name, last_name, title, department, salary, manager)
 VALUES ("Malia", "Brown", "Accountant", "Finance", 125000, "Ashley Rodriguez");
 
-INSERT INTO music (first_name, last_name, title, department, salary, manager)
+INSERT INTO employee (first_name, last_name, title, department, salary, manager)
 VALUES ("Sarah", "Lourd", "Legal Team Lead", "Legal", 250000, "Ashley Rodriguez");
 
 -- UPDATE music 
