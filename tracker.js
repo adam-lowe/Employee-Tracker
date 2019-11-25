ca5c539c914218c18cfd98996868a0bd5f483547
Goldpm <lowedavidadam@gmail.com>
2019-11-24 21:50:32 -0500

    added sql connection and inquirer

---
+++ tracker.js
@@ -0,0 +1,41 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table")
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "rootroot",
+  database: "employees_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+});
+
+inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+      },
+      {
+        type: "input",
+        name: "location",
+        message: "Where are you from?"
+      },
+      {
+        type: "input",
+        name: "hobby",
+        message: "What is your favorite hobby?"
+      },
+      
+    ]);
