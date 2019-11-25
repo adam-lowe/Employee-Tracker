450b4884c20ef1a613d972a13e25e48749fa528a
Goldpm <lowedavidadam@gmail.com>
2019-11-24 22:51:29 -0500

    added inquirer questions base

--- tracker.js
+++ tracker.js
@@ -36,29 +36,71 @@ console.log(
     "██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║       \n",
      "╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝       \n");
 
-inquirer.prompt([
-{
-    type: "input",
-    name: "gituser",
-    message: "What is your github username?"
-},
-{
-    type: "list",
-    message: "What is your favorite color?",
-    name: "color",
-    choices: [
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "brown",
-    "purple",
-    "blue"
-    ]
+inquirerAction();
+
+
+function inquirerAction() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "action",
+            choices: [
+                "View all employees",
+                "View all employees By department",
+                "View all employees By manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager"
+            ]
+        }
+    ]).then(function (response) {
+        const action = response.action;
+        switch (action) {
+            case "View all employees":
+                break;
+            case "View all employees By department":
+                break;
+            case "View all employees By manager":
+                break;
+            case "Add Employee":
+                break;
+            case "Remove Employee":
+                break;
+            case "Update Employee Role":
+                break;
+            case "Update Employee Manager":
+                break;
+            default:
+                console.log("Error: Can't Determine Choice")
+                break;
+        }
+    });
 }
-]).then(function (response) {
-const username = response.gituser;
-const favcolor = response.color;
-});
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         name: "gituser",
+//         message: "What is your github username?"
+//     },
+//     {
+//         type: "list",
+//         message: "What is your favorite color?",
+//         name: "color",
+//         choices: [
+//         "red",
+//         "orange",
+//         "yellow",
+//         "green",
+//         "brown",
+//         "purple",
+//         "blue"
+//         ]
+//     }
+// ]).then(function (response) {
+// const username = response.gituser;
+// const favcolor = response.color;
+// });
 // const queryUrl = `https://api.github.com/users/${username}`;
 // axios.get(queryUrl)
